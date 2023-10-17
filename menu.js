c9b4b29f2c38a56a17475030e527c73cbe441f78
Dana <144537942+danad1821@users.noreply.github.com>
2023-10-17 20:56:56 +0300

    Add files via upload

--- menu.js
+++ menu.js
@@ -13,7 +13,7 @@
 let bottom = document.documentElement.scrollHeight;
 let upDown = document.getElementsByClassName("up-down-btns");
 let upBtn = document.getElementById("up-btn");
-let addItems=document.getElementsByClassName("add-btn");
+let addItems = document.getElementsByClassName("add-btn");
 function goUp() {
     window.scrollTo(0, 0);
 }
@@ -38,46 +38,73 @@ window.addEventListener("scroll", function () {
 
 upDown[0].addEventListener("click", goUp)
 upDown[1].addEventListener("click", goDown)
-// addItems.array.forEach(element => {
-//     element.addEventListener("click", function(){
-//         for(let i=0; i<addItems.length; i++){
-//             let quantity=getElementsByClassName("menu-item-quantity")[i];
-//             if(quantity>0){
-//                 let name=getElementsByClassName("menu-item-name")[i];
-//                 let price=getElementsByClassName("menu-item-price")[i];
-//                 newItem= new menuItem(name, price, quantity);
-//                 items.append(newItem);
-//             }
-//         }
-//     })
-// });
-var menu = JSON.parse(menu);
-let menuList=document.getElementsByClassName("menu")[0];
-menu.forEach(element => {
-    let sectionElement=document.createElement("div");
-    sectionElement.className="menu-sections";
-    let sectionTitle=document.createElement("h2");
-    sectionTitle.className="section-title";
-    sectionTitle.textContent=element.name;
-    sectionElement.appendChild(sectionTitle);
-    let content=document.createElement("div");
-    content.className="menu-items";
-    element.items.forEach(item=>{
-        let innerContent=document.createElement("div");
-        innerContent.className="menu-item";
-        let image=document.createElement("img");
-        image.className="img-menu-item";
-        image.src=item.img;
-        innerContent.appendChild(image);
-        let title=document.createElement("h4");
-        title.textContent=item.name;
-        title.className="menu-item-name";
-        innerContent.append(title);
-        let description=document.createElement("p");
-        description.textContent="Description: "+item.description;
-        description.className="menu-item-description"
-        let price=document.createElement("p")
+
+var menulist = document.getElementById("menu");
+fetch('menu.json')
+    .then(response => response.json())
+    .then(data => {
+        data.menu.forEach(item => {
+            let sectionElement = document.createElement("div");
+            sectionElement.classList.add("menu-sections");
+            let sectionTitle = document.createElement("h2");
+            sectionTitle.classList.add("section-title");
+            sectionTitle.id=item.id;
+            sectionTitle.textContent = item.sectionName;
+            sectionElement.appendChild(sectionTitle);
+            let content = document.createElement("div");
+            content.classList.add("menu-items");
+            item.items.forEach(item => {
+                if (item.subName === undefined) {
+                    let innerContent = document.createElement("div");
+                    innerContent.classList.add("menu-item");
+                    innerContent.innerHTML =
+                        `
+                    <img class="img-menu-item"src="${item.img}"">
+                    <h4 class="menu-item-name">${item.itemName}</h4>
+                    <p class="menu-item-description"><b>Description:</b> ${item.description}</p>
+                    <p class="menu-item-price"><b>Price:</b> $${item.price}</p>
+                    <div class="menu-item-details">
+                        <label for="quantity" class="quantity-title">Quantity:</label>
+                        <input type="number" class="menu-item-quantity" min="0" max="10" placeholder="0">
+                        <button class="add-btn">+</button>
+                    </div>
+                `
+                    content.appendChild(innerContent);
+                }
+                else{
+                    let category=document.createElement("div");
+                    category.classList.add("menu-items-with-categories");
+                    let subtitle=document.createElement("h3");
+                    subtitle.classList.add("subsection-title");
+                    subtitle.id=item.id;
+                    subtitle.textContent=item.subName
+                    category.appendChild(subtitle)
+                    let subsection=document.createElement("div");
+                    subsection.classList.add("menu-subsection");
+                    item.subItems.forEach(subItem=>{
+                        let innerContent = document.createElement("div");
+                        innerContent.classList.add("menu-item");
+                        innerContent.innerHTML =
+                        `
+                        <img class="img-menu-item"src="${subItem.img}"">
+                        <h4 class="menu-item-name">${subItem.itemName}</h4>
+                        <p class="menu-item-description"><b>Description:</b> ${subItem.description}</p>
+                        <p class="menu-item-price"><b>Price:</b> $${subItem.price}</p>
+                        <div class="menu-item-details">
+                            <label for="quantity" class="quantity-title">Quantity:</label>
+                            <input type="number" class="menu-item-quantity" min="0" max="10" placeholder="0">
+                            <button class="add-btn">+</button>
+                        </div>
+                        `
+                        subsection.appendChild(innerContent)
+                    })
+                    category.appendChild(subsection)
+                    content.appendChild(category)
+                }
+            })
+            sectionElement.appendChild(content)
+            menulist.appendChild(sectionElement);
+        });
+        bottom = document.documentElement.scrollHeight;
     })
-    
-    
-});
+    .catch(error => console.log('Error:', error));
