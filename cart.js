4fb77b84b964d40be24ab094fd4b5e3fb7ca2f1f
Dana <144537942+danad1821@users.noreply.github.com>
2023-11-13 17:05:53 +0200

    Add files via upload

--- cart.js
+++ cart.js
@@ -4,6 +4,59 @@ let ordered = JSON.parse(window.sessionStorage.getItem('order'))
 let menuI = JSON.parse(window.sessionStorage.getItem('menu'))
 let displayOfTotal = document.getElementsByClassName("price-total")[0];
 let tot = 0;
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = { 37: 1, 38: 1, 39: 1, 40: 1, 33: 1, 34: 1, 35: 1, 36: 1 };
+
+function preventDefaultt(e) {
+    e.preventDefault();
+}
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefaultt(e);
+        return false;
+    }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+    window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+        get: function () { supportsPassive = true; }
+    }));
+} catch (e) { }
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+    window.addEventListener('DOMMouseScroll', preventDefaultt, false); // older FF
+    window.addEventListener(wheelEvent, preventDefaultt, wheelOpt); // modern desktop
+    window.addEventListener('touchmove', preventDefaultt, wheelOpt); // mobile
+    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+    window.removeEventListener('DOMMouseScroll', preventDefaultt, false);
+    window.removeEventListener(wheelEvent, preventDefaultt, wheelOpt);
+    window.removeEventListener('touchmove', preventDefaultt, wheelOpt);
+    window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+let popContainer = document.getElementsByClassName("pop-up-container")[0];
+let popUpMsg = document.getElementsByClassName("pop-up")[0];
+
+function popUp() {
+    disableScroll();
+    popContainer.style.display = "flex";
+    popContainer.style.alignItems = "center";
+    popContainer.style.justifyContent = "center";
+    popUpMsg.style.display = "flex";
+    popUpMsg.style.flexDirection = "column";
+}
+
 for (let i = 0; i < ordered.length; i++) {
     let row = document.createElement("tr");
     row.classList.add("tableBody-row")
@@ -25,16 +78,19 @@ let foodName = document.querySelectorAll("td:nth-child(1)")
 for (let j = 0; j < qtNum.length; j++) {
     qtNum[j].addEventListener("change", () => {
         if (qtNum[j].value == 0) {
-            var removeitem = confirm("Are you sure you want to remove this item?")
-            if (removeitem) {
+            popUp()
+            document.getElementById("yes").addEventListener("click", () => {
                 ordered.splice(j, 1);
                 window.sessionStorage.setItem('order', JSON.stringify(ordered))
                 location.reload()
                 j = 0
-            }
-            else {
+            });
+            document.getElementById("no").addEventListener("click", () => {
+                enableScroll();
+                popContainer.style.display = "none";
+                popUpMsg.style.display = "none";
                 qtNum[j].value = 1;
-            }
+            })
         }
         else {
             let ogPrice = 0;
@@ -46,7 +102,7 @@ for (let j = 0; j < qtNum.length; j++) {
             let tPrice = ogPrice * parseFloat(qtNum[j].value);
             document.querySelectorAll(".price")[j].textContent = "$" + tPrice.toFixed(2);
             ordered[j].price = tPrice;
-            ordered[j].quantity=qtNum[j].value;
+            ordered[j].quantity = qtNum[j].value;
             window.sessionStorage.setItem('order', JSON.stringify(ordered))
         }
         tot = 0;
@@ -58,36 +114,51 @@ for (let j = 0; j < qtNum.length; j++) {
     })
 }
 
+
+
+let closePopUp = document.getElementById("close-btn");
+closePopUp.addEventListener("click", () => {
+    enableScroll();
+    popContainer.style.display = "none";
+    popUpMsg.style.display = "none";
+})
+
 let removeBtn = document.getElementsByClassName("remove-btn");
 for (let l = 0; l < removeBtn.length; l++) {
     removeBtn[l].addEventListener("click", () => {
-        var removeitem = confirm("Are you sure you want to remove this item?")
-        if (removeitem) {
+        popUp()
+        let yesBtn = document.getElementById("yes");
+        yesBtn.addEventListener("click", () => {
             ordered.splice(l, 1);
             window.sessionStorage.setItem('order', JSON.stringify(ordered))
             location.reload()
-        }
+        });
+        document.getElementById("no").addEventListener("click", () => {
+            enableScroll();
+            popContainer.style.display = "none";
+            popUpMsg.style.display = "none";
+        })
     })
 }
-window.addEventListener("load", ()=>{
-    let tableBody=document.getElementById("cart-items-body");
-    let emptyCart=document.getElementById("empty-cart");
-    let totalDiv=document.getElementById("total-div");
-    if(tableBody.childElementCount==0){
-        tableBody.parentElement.style.display="none"
-        totalDiv.style.display="none"
-        emptyCart.style.display="block"
+window.addEventListener("load", () => {
+    let tableBody = document.getElementById("cart-items-body");
+    let emptyCart = document.getElementById("empty-cart");
+    let totalDiv = document.getElementById("total-div");
+    if (tableBody.childElementCount == 0) {
+        tableBody.parentElement.style.display = "none"
+        totalDiv.style.display = "none"
+        emptyCart.style.display = "block"
     }
-    else{
-        tableBody.parentElement.style.display="table"
-        totalDiv.style.display="flex"
-        totalDiv.style.flexDirection="row"
-        totalDiv.style.alignItems="flex-end"
-        emptyCart.style.display="none"
+    else {
+        tableBody.parentElement.style.display = "table"
+        totalDiv.style.display = "flex"
+        totalDiv.style.flexDirection = "row"
+        totalDiv.style.alignItems = "flex-end"
+        emptyCart.style.display = "none"
     }
 })
-let checkoutBtn=document.getElementsByClassName("checkout-btn")[0];
-checkoutBtn.addEventListener("click", ()=>{
+let checkoutBtn = document.getElementsByClassName("checkout-btn")[0];
+checkoutBtn.addEventListener("click", () => {
     if (window.sessionStorage.getItem("SignedIn") == "true") {
         // if (ordery.length!=0){
         window.open("checkout.html", "_self");
